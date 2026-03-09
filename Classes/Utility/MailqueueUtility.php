38db7cfae5ad1b0efc789106d395122d6675739c
Daniel Piffl <dp@dpiffl.de>
2026-03-09 16:00:27 +0100

    Comment out unnecessary query for storagePid and set it fix to 3 (old value)

--- Classes/Utility/MailqueueUtility.php
+++ Classes/Utility/MailqueueUtility.php
@@ -4,40 +4,44 @@ namespace WEBprofil\WpMailqueue\Utility;
 
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
-use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 use WEBprofil\WpMailqueue\Domain\Model\Mail;
+use Doctrine\DBAL\Query\QueryBuilder;
 
-class MailqueueUtility
-{
-    protected static $table = 'tx_wpmailqueue_domain_model_mail';
-
-    public static function addToMailqueue(Mail $mail)
-    {
-        $configurationManager = GeneralUtility::makeInstance(ConfigurationManager::class);
-        $settings = $configurationManager->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK, 'WpMailqueue');
-
-        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
-            ->getQueryBuilderForTable(self::$table);
-
-        $queryBuilder
-            ->insert(self::$table)
-            ->values(
-                [
-                    'pid' => (int)($settings['persistence']['storagePid'] ?? 0),
-                    'crdate' => time(),
-                    'tstamp' => time(),
-                    'sender' => $mail->getSender(),
-                    'subject' => $mail->getSubject(),
-                    'body' => $mail->getBody(),
-                    'body_html' => $mail->getBodyHtml(),
-                    'recipient' => $mail->getRecipient(),
-                    'cc' => $mail->getCc(),
-                    'bcc' => $mail->getBcc(),
-                    'attachements' => $mail->getAttachements(),
-                    'type' => $mail->getType()
-                ]
-            )
-            ->executeStatement();
-    }
+class MailqueueUtility {
+	
+	protected static $table = 'tx_wpmailqueue_domain_model_mail';
+	
+	public static function addToMailqueue(Mail $mail) {
+		
+		$storagePid = 3;
+// 		try {
+// 			$configurationManager = GeneralUtility::makeInstance(ConfigurationManager::class);
+// 			$settings = $configurationManager->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK, 'WpMailqueue');
+			
+// 			$storagePid = (int)($settings['persistence']['storagePid'] ?? 0);
+// 		}
+// 		catch( \Throwable $throwable ){	}
+		
+		/** @var ConnectionPool $connectionPool */
+		$connectionPool = GeneralUtility::makeInstance( ConnectionPool::class );
+		
+		/** @var QueryBuilder $queryBuilder */
+		$queryBuilder = $connectionPool->getQueryBuilderForTable( self::$table );
+		
+		$queryBuilder->insert( self::$table )
+			->values([ 
+				'pid' => $storagePid,
+				'crdate' => time(),
+				'tstamp' => time(),
+				'sender' => $mail->getSender(),
+				'subject' => $mail->getSubject(),
+				'body' => $mail->getBody(),
+				'body_html' => $mail->getBodyHtml(),
+				'recipient' => $mail->getRecipient(),
+				'cc' => $mail->getCc(),
+				'bcc' => $mail->getBcc(),
+				'attachements' => $mail->getAttachements(),
+				'type' => $mail->getType()
+		])->executeStatement();
+	}
 }
